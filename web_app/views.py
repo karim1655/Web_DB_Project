0acaf8d660a91faa1ded102db4e7263c8ca92920
karim_desktop <damorekarim7@gmail.com>
2024-12-11 00:36:39 +0100

    Added register, login, and logout views

--- web_app/views.py
+++ web_app/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import CustomUserCreationForm
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'web_app/home.html', context={'title': 'home', })
+    return render(request, 'web_app/home.html', context={'title': 'Home', })
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'registration/logged_out.html'
