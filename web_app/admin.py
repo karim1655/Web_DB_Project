5edf461d973ee920e271f117227ab756d3b26c1b
karim_desktop <damorekarim7@gmail.com>
2024-12-10 17:24:29 +0100

    Registered models in admin.py

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+admin.site.register(models.CustomUser)
+admin.site.register(models.TrainingPlan)
+admin.site.register(models.Relazione)
